c2da5c65a503d0a42410e0dca5a4522f9fbd999b
Mator <why101@msn.com>
2025-12-08 17:46:56 -0800

    remove unused keywords and expressions at runtime

--- packages/sigil-sifter-card-magician/src/index.js
+++ packages/sigil-sifter-card-magician/src/index.js
@@ -1,5 +1,37 @@
 import Template from './keywords/Template.js';
+import CardMagicianCard from './CardMagicianCard.js';
+
+const keywordsToRemove = [
+    'BannedKeyword', 'FormatKeyword', 'GameKeyword', 'RestrictedKeyword',
+    'SetKeyword', 'SetTypeKeyword'
+];
+
+const isExpressionsToRemove = [
+    'BoosterExpression', 'PromoExpression', 'ReprintExpression',
+    'ReservedExpression', 'SpotlightExpression'
+];
+
+function removeUnusedKeywords(sifter) {
+    keywordsToRemove.forEach(key => {
+        delete sifter.keywords[key];
+    });
+}
+
+function patchIsKeyword(sifter) {
+    const oldExprs = sifter.keywords.IsKeyword.supportedExpressions;
+    const newExprs = oldExprs.filter(k => {
+        return !isExpressionsToRemove.includes(k.name);
+    });
+    Object.defineProperty(sifter.keywords.IsKeyword, 'supportedExpressions', {
+        configurable: true,
+        enumerable: true,
+        get: () => newExprs
+    });
+}
 
 export default function register(sifter) {
+    removeUnusedKeywords(sifter);
+    patchIsKeyword(sifter);
     sifter.addKeywords([Template]);
+    sifter.setInputAdapter(CardMagicianCard);
 }
